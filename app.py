2e741b95651fafd0946e0ab589ebd40c00d0946e
RobbyArsyadani <100019313+RobbyArsyadani@users.noreply.github.com>
2022-02-23 03:54:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import math
+from flask import Flask, render_template,request
 
 app = Flask (__name__)
 @app.route('/')
@@ -6,12 +7,22 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/pitagoras')
+@app.route('/pitagoras', methods=['GET','POST'])
 def pitagoras():
+    if request.method == 'POST':
+        angka= request.form['front']
+        angka2= request.form['side']
+        result=math.sqrt(int(angka)**2+int(angka2)**2)
+
+        return render_template('pitagoras_view.html',num1=int(angka),num2=int(angka2),result=int(result))
     return render_template('pitagoras.html')
 
-@app.route('/squareroot')
+@app.route('/squareroot',methods=['GET','POST'])
 def squareroot():
+    if request.method == 'POST':
+        num = request.form ['angka']
+        hasil = math.sqrt(int(num))
+        return render_template('squareroot_view.html',num = int(num),hasil=int(hasil))
     return render_template('squareroot.html')
 
 
@@ -21,6 +32,6 @@ def convert():
 
 if __name__ == '__main__':
 
-    app.run()
+    app.run(debug=True)
 
     
